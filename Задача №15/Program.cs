5ae01d4816ec9ebe4f55367492e8ba20533201a2
Andrey <man.liher66@gmail.com>
2022-11-16 20:58:39 +0300

    Задача №15

---
+++ Задача №15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число от 1 до 7 согласно дню недели");
+string number = Console.ReadLine();
+int a = int.Parse(number);
+
+if (a >= 6 && a <= 7)
+{
+    Console.WriteLine("Да, выходной день");
+}
+else if (a >= 8 || a <= 0)
+{
+    Console.WriteLine("Попробуй еще раз");
+}
+else
+{
+    Console.WriteLine("Нет, не выходной");
+}
