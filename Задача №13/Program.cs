1fe53310df49787551a92c6ac8fca7c1330eea3c
Andrey <man.liher66@gmail.com>
2022-11-16 20:10:34 +0300

    Задача №13

---
+++ Задача №13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int a = int.Parse(number);
+
+if (a >= 100)
+{
+    while (a > 999)
+    {
+        a = a / 10;
+    }
+    int third = a % 10;
+    Console.WriteLine("Третье число " + third);
+}   
+else
+{
+Console.WriteLine("Третьего числа нет");
+}    
