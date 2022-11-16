98ccef46099e9b37fd82758330ea62e957c44bcc
Andrey <man.liher66@gmail.com>
2022-11-16 19:21:15 +0300

    Задача №10

---
+++ Задача №10/Program.cs
@@ -0,0 +1,14 @@
+﻿// int number = new Random().Next(100, 1000);
+// Console.WriteLine(number);
+// int first = number % 100;
+// int second = first / 10;
+
+// Console.WriteLine("Числа: " + second);
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int a = int.Parse(number);
+int first = a % 100;
+int second = first / 10;
+
+Console.WriteLine("Число " + second);
